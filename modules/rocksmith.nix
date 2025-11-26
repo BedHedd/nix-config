8e7b04b32eeadbc428f674b699a7ce90dbcb65a8
bedhedd <zbedhedd@gmail.com>
2025-11-26 17:49:12 -0600

    update rocksmith module

--- modules/rocksmith.nix
+++ modules/rocksmith.nix
@@ -1,6 +1,17 @@
+# modules/rocksmith.nix
 { config, pkgs, lib, username, ... }:
 
+let
+  # Import nixos-rocksmith via default.nix, no flake input needed
+  nixos-rocksmith =
+    import (builtins.fetchTarball "https://github.com/re1n0/nixos-rocksmith/archive/master.tar.gz") {};
+in
 {
+  # Pull in the upstream module so the option exists
+  imports = [
+    nixos-rocksmith.nixosModules.default
+  ];
+
   ### Audio
   services.pipewire.enable = lib.mkDefault true;
 
@@ -12,7 +23,7 @@
     (config.environment.systemPackages or []) ++
     (with pkgs; [ helvum rtaudio ]);
 
-  ### Steam
+  ### Steam + Rocksmith
   programs.steam = {
     rocksmithPatch.enable = true;
   };
