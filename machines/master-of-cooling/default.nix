91b8358089d2b7f2903bc0daf5072c37a8931dca
bedhedd <zbedhedd@gmail.com>
2025-11-29 13:48:52 -0600

    update virtualization

--- machines/master-of-cooling/default.nix
+++ machines/master-of-cooling/default.nix
@@ -137,5 +137,9 @@
   };
 
   virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
+  users.extraGroups.vboxusers.members = [ "bedhedd" ];
+  virtualisation.virtualbox.host.enableHardening = false;
 
 }
